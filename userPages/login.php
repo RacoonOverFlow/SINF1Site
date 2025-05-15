6f6e8f1e39531c90029a5dd62fbcbc34d70e8185
josemrp <1230650@isep.ipp.pt>
2025-05-15 10:31:52 +0100

    .

--- userPages/login.php
+++ userPages/login.php
@@ -94,7 +94,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
   </header>
 
-  <div class="wrapper">
+<div class="wrapper">
+  <div class="login-box">
     <h2 class="loginTitle">Quag-in</h2>
     <p>Please fill in your credentials to login.</p>
 
@@ -105,25 +106,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ?>
 
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-      <div class="f</form>orm-group">
+      <div class="form-group">
         <label>Username</label>
-       </div> <input type="text" name="username"
+        <input type="text" name="username"
           class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
           value="<?php echo $username; ?>">
         <span class="invalid-feedback"><?php echo $username_err; ?></span>
       </div>
+
       <div class="form-group">
         <label>Password</label>
         <input type="password" name="password"
           class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
         <span class="invalid-feedback"><?php echo $password_err; ?></span>
       </div>
+
       <div class="form-group">
         <input type="submit" class="btn btn-primary" value="Login">
       </div>
       <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
     </form>
   </div>
+</div>
+
 </body>
 </php>
 <script src="../js/mainPage.js"></script>
